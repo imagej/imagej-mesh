bf1e08328d24bd3b04b678996db68a735db7a3d7
Curtis Rueden <ctrueden@wisc.edu>
2018-03-27 11:53:17 -0500

    Migrate PLY I/O to new mesh I/O plugin style
    The package is now net.imagej.mesh.io.ply.
    
    This is an initial update that tries to stay close to the previous API.

---
+++ src/main/java/net/imagej/mesh/io/ply/PLYMeshIO.java
@@ -0,0 +1,271 @@
+
+package net.imagej.mesh.io.ply;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import net.imagej.mesh.Mesh;
+import net.imagej.mesh.Triangle;
+import net.imagej.mesh.Vertex;
+import net.imagej.mesh.io.MeshIOPlugin;
+import net.imagej.mesh.naive.NaiveMesh;
+
+import org.scijava.io.AbstractIOPlugin;
+import org.scijava.plugin.Plugin;
+import org.scijava.util.FileUtils;
+import org.smurn.jply.Element;
+import org.smurn.jply.ElementReader;
+import org.smurn.jply.PlyReader;
+import org.smurn.jply.PlyReaderFile;
+import org.smurn.jply.util.NormalMode;
+import org.smurn.jply.util.NormalizingPlyReader;
+import org.smurn.jply.util.TesselationMode;
+import org.smurn.jply.util.TextureMode;
+
+import gnu.trove.map.hash.TIntLongHashMap;
+import gnu.trove.map.hash.TLongIntHashMap;
+
+/**
+ * A plugin for reading and writing <a href=
+ * "http://www.cs.virginia.edu/~gfx/Courses/2001/Advanced.spring.01/plylib/Ply.txt">PLY
+ * files</a>.
+ * 
+ * @author Kyle Harrington (University of Idaho, Moscow)
+ * @author Curtis Rueden
+ */
+@Plugin(type = MeshIOPlugin.class)
+public class PLYMeshIO extends AbstractIOPlugin<Mesh> implements MeshIOPlugin {
+
+	// -- PLYMeshIO methods --
+
+	public void read(final File plyFile, final Mesh mesh) throws IOException {
+		PlyReader plyReader = new PlyReaderFile(plyFile);
+		plyReader = new NormalizingPlyReader(plyReader, TesselationMode.TRIANGLES,
+			NormalMode.ADD_NORMALS_CCW, TextureMode.PASS_THROUGH);
+
+		ElementReader reader = plyReader.nextElementReader();
+
+		final TIntLongHashMap rowToVertIndex = new TIntLongHashMap();
+		int vertCount = 0;
+
+		// First read vertices so we have all indices, maybe this could be more
+		// efficient
+		while (reader != null) {
+			if (reader.getElementType().getName().equals("vertex")) {
+				Element vertex = reader.readElement();
+				while (vertex != null) {
+					float x = (float) vertex.getDouble("x");
+					float z = (float) vertex.getDouble("z");
+					float y = (float) vertex.getDouble("y");
+					float nx = (float) vertex.getDouble("nx");
+					float ny = (float) vertex.getDouble("ny");
+					float nz = (float) vertex.getDouble("nz");
+					float u = 0, v = 0, w = 0; // TODO: texture coordinate
+					final long vIndex = mesh.vertices().addf(x, y, z, nx, ny, nz, u, v, w);
+					rowToVertIndex.put(vertCount++, vIndex);
+					vertex = reader.readElement();
+				}
+			}
+			reader.close();
+			reader = plyReader.nextElementReader();
+		}
+		plyReader.close();
+
+		// Now read faces
+		plyReader = new PlyReaderFile(plyFile);
+		plyReader = new NormalizingPlyReader(plyReader, TesselationMode.TRIANGLES,
+			NormalMode.ADD_NORMALS_CCW, TextureMode.PASS_THROUGH);
+
+		reader = plyReader.nextElementReader();
+
+		// First read vertices so we have all indices.
+		// Maybe this could be more efficient.
+		while (reader != null) {
+			if (reader.getElementType().getName().equals("face")) {
+				Element triangle = reader.readElement();
+				while (triangle != null) {
+					final int[] indices = triangle.getIntList("vertex_index");
+					final long v1 = rowToVertIndex.get(indices[0]);
+					final long v2 = rowToVertIndex.get(indices[1]);
+					final long v3 = rowToVertIndex.get(indices[2]);
+					final float nx = 0, ny = 0, nz = 0;
+					mesh.triangles().add(v1, v2, v3, nx, ny, nz);
+					triangle = reader.readElement();
+				}
+			}
+			reader.close();
+			reader = plyReader.nextElementReader();
+		}
+
+		plyReader.close();
+	}
+
+	public byte[] writeBinary(final Mesh mesh) {
+		final int vertexBytes = 3 * 4 + 3 * 4 + 3 * 4;
+		final int triangleBytes = 3 * 4 + 1;
+		final String header = "ply\n" + //
+			"format binary_little_endian 1.0\n" + //
+			"comment This binary PLY mesh was created with imagej-mesh.\n";
+		final String vertexHeader = "" + //
+			"element vertex " + mesh.vertices().size() + "\n" + //
+			"property float x\nproperty float y\nproperty float z\n" + //
+			"property float nx\nproperty float ny\nproperty float nz\n" + //
+			"property float r\n property float g\n property float b\n";
+		final String triangleHeader = "element face " + mesh.triangles().size() +
+			"\nproperty list uchar int vertex_index\n";
+		final String endHeader = "end_header\n";
+		final long bytes = header.getBytes().length + //
+			vertexHeader.getBytes().length + triangleHeader.getBytes().length +
+			endHeader.getBytes().length + mesh.vertices().size() * vertexBytes + //
+			mesh.triangles().size() * triangleBytes;
+		if (bytes > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("Mesh data too large: " + bytes);
+		}
+		final ByteBuffer buffer = ByteBuffer.allocate((int) bytes).order(
+			ByteOrder.LITTLE_ENDIAN);
+
+		buffer.put(header.getBytes());
+		buffer.put(vertexHeader.getBytes());
+		buffer.put(triangleHeader.getBytes());
+		buffer.put(endHeader.getBytes());
+
+		// Do not populate file if there are no vertices
+		if (mesh.vertices().size() == 0) {
+			return buffer.array();
+		}
+
+		// Write vertices
+		final TLongIntHashMap refToVertId = //
+			new TLongIntHashMap((int) mesh.vertices().size());
+		int vertId = 0;
+		for (final Vertex v : mesh.vertices()) {
+			buffer.putFloat(v.xf());
+			buffer.putFloat(v.yf());
+			buffer.putFloat(v.zf());
+			buffer.putFloat(v.nxf());
+			buffer.putFloat(v.nyf());
+			buffer.putFloat(v.nzf());
+			buffer.putFloat(v.uf());
+			buffer.putFloat(v.vf());
+			buffer.putFloat(v.wf());
+			refToVertId.put(v.index(), vertId);
+			++vertId;
+		}
+
+		// Write triangles
+		for (final Triangle t : mesh.triangles()) {
+			buffer.put((byte) 3);
+			buffer.putInt(refToVertId.get(t.vertex0()));
+			buffer.putInt(refToVertId.get(t.vertex1()));
+			buffer.putInt(refToVertId.get(t.vertex2()));
+		}
+
+		return buffer.array();
+	}
+
+	public byte[] writeAscii(final Mesh mesh) throws IOException {
+		final String header =
+			"ply\nformat ascii 1.0\ncomment This binary PLY mesh was created with imagej-mesh.\n";
+		final String vertexHeader = "element vertex " + mesh.vertices().size() +
+			"\nproperty float x\nproperty float y\nproperty float z\nproperty float nx\nproperty float ny\nproperty float nz\nproperty float r\n property float g\n property float b\n";
+		final String triangleHeader = "element face " + mesh.triangles().size() +
+			"\nproperty list uchar int vertex_index\n";
+		final String endHeader = "end_header\n";
+
+		// TODO: Fail fast more robustly if mesh is too large.
+		// But need to modify the API to not return a byte[].
+		if (mesh.vertices().size() > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("Too many vertices: " + //
+				mesh.vertices().size());
+		}
+		if (mesh.triangles().size() > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("Too many triangles: " + //
+				mesh.triangles().size());
+		}
+
+		final ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+		final Writer writer = new OutputStreamWriter(os, "UTF-8");
+
+		writer.write(header);
+		writer.write(vertexHeader);
+		writer.write(triangleHeader);
+		writer.write(endHeader);
+
+		// Do not populate file if there are no vertices
+		if (mesh.vertices().size() == 0) {
+			writer.flush();
+			return os.toByteArray();
+		}
+
+		// Write vertices
+		final TLongIntHashMap refToVertId = new TLongIntHashMap(//
+			(int) mesh.vertices().size());
+		int vertId = 0;
+		for (final Vertex v : mesh.vertices()) {
+			writer.write(Float.toString(v.xf()));
+			writer.write(' ');
+			writer.write(Float.toString(v.yf()));
+			writer.write(' ');
+			writer.write(Float.toString(v.zf()));
+			writer.write(' ');
+			writer.write(Float.toString(v.nxf()));
+			writer.write(' ');
+			writer.write(Float.toString(v.nyf()));
+			writer.write(' ');
+			writer.write(Float.toString(v.nzf()));
+			writer.write(' ');
+			writer.write(Float.toString(v.uf()));
+			writer.write(' ');
+			writer.write(Float.toString(v.vf()));
+			writer.write(' ');
+			writer.write(Float.toString(v.wf()));
+			writer.write('\n');
+			refToVertId.put(v.index(), vertId);
+			++vertId;
+		}
+
+		// Write triangles
+		for (final Triangle t : mesh.triangles()) {
+			writer.write("3 ");
+			writer.write(Integer.toString(refToVertId.get(t.vertex0())));
+			writer.write(' ');
+			writer.write(Integer.toString(refToVertId.get(t.vertex1())));
+			writer.write(' ');
+			writer.write(Integer.toString(refToVertId.get(t.vertex2())));
+			writer.write('\n');
+		}
+		writer.flush();
+		return os.toByteArray();
+	}
+
+	// -- IOPlugin methods --
+
+	@Override
+	public boolean supportsOpen(final String source) {
+		return true;
+	}
+
+	@Override
+	public boolean supportsSave(final String source) {
+		return true;
+	}
+
+	@Override
+	public Mesh open(final String source) throws IOException {
+		final Mesh mesh = new NaiveMesh();
+		read(new File(source), mesh);
+		return mesh;
+	}
+
+	@Override
+	public void save(final Mesh data, final String destination) throws IOException {
+		final byte[] bytes = writeBinary(data);
+		FileUtils.writeFile(new File(destination), bytes);
+	}
+}
---
+++ src/test/java/net/imagej/mesh/io/ply/PLYMeshIOTest.java
@@ -0,0 +1,57 @@
+
+package net.imagej.mesh.io.ply;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assume.assumeTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import net.imagej.mesh.Mesh;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link PLYMeshIO}.
+ *
+ * @author Kyle Harrington (University of Idaho, Moscow)
+ * @author Curtis Rueden
+ */
+public class PLYMeshIOTest {
+
+	@Test
+	public void testOpen() throws Exception {
+		final PLYMeshIO meshIO = new PLYMeshIO();
+		final Mesh m = sampleMesh(meshIO);
+		assertEquals(158, m.triangles().size());
+		assertEquals(81, m.vertices().size());
+	}
+
+	@Test
+	public void testBinaryWrite() throws Exception {
+		final PLYMeshIO meshIO = new PLYMeshIO();
+		final Mesh mesh = sampleMesh(meshIO);
+		final byte[] bytes = meshIO.writeBinary(mesh);
+		assertEquals(5306, bytes.length);
+	}
+
+	@Test
+	public void testAsciiWrite() throws Exception {
+		final PLYMeshIO meshIO = new PLYMeshIO();
+		final Mesh mesh = sampleMesh(meshIO);
+		final byte[] bytes = meshIO.writeAscii(mesh);
+		assertEquals(7762, bytes.length);
+	}
+
+	// -- Helper methods --
+
+	private Mesh sampleMesh(final PLYMeshIO meshIO) throws URISyntaxException,
+		IOException
+	{
+		final URI meshURI = getClass().getResource("cone.ply").toURI();
+		assumeTrue(new File(meshURI).exists());
+		return meshIO.open(meshURI.getPath());
+	}
+}
